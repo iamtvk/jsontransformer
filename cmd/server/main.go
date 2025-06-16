475c70bcbf18acf9f71fb8755edd6f780e5bb95b
vamshikrishna@thinklab.com.au <vamshikrishna@thinklab.com.au>
2025-06-16 11:21:17 +0000

    add protobuff for transform

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,17 +2,17 @@ package main
 
 import (
 	"database/sql"
-	"log"
-
 	"github.com/iamtvk/jsontransformer/internal/config"
 	"github.com/iamtvk/jsontransformer/internal/repository/postgres"
 	"github.com/iamtvk/jsontransformer/internal/service"
-	"github.com/iamtvk/jsontransformer/internal/transport/http"
+	httptransport "github.com/iamtvk/jsontransformer/internal/transport/http"
+	"log"
+	"net/http"
 )
 
 func main() {
 	cfg := config.Load()
-	db, err := sql.Open("postgre", cfg.DbUrl)
+	db, err := sql.Open("postgres", cfg.DbUrl)
 	if err != nil {
 		log.Fatalf(err.Error())
 	}
@@ -20,8 +20,7 @@ func main() {
 	cache := service.NewCacheLayer()
 	repo := postgres.NewPostgreSQLRepository(db)
 	transformerService := service.NewTransformerService(repo, cfg, cache)
+	httptransport.NewHandler(transformerService)
+	http.Handle()
 
-	httpHandler := http.NewHandler(transformerService)
-
-	httpHandler.Transform()
 }
